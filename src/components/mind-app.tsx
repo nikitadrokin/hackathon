71bdba83c593e7e8b84b21b4492325062b3d0c5d
Nikita D <dnawastaken@gmail.com>
2026-03-31 15:33:54 -0700

    deleted project for now

--- src/components/mind-app.tsx
+++
@@ -1,1 +0,0 @@
-export { default } from "./mind/mind-app";
--- src/shims/node-stream-web.ts
+++
@@ -1,9 +0,0 @@
-export const ReadableStream = globalThis.ReadableStream;
-export const TransformStream = globalThis.TransformStream;
-export const WritableStream = globalThis.WritableStream;
-
-export default {
-	ReadableStream,
-	TransformStream,
-	WritableStream,
-};
